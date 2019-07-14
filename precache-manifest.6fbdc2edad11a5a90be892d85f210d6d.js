70ea914df9b108da13a321f81323fcfc4fa12b68
Hakeem Shrief <abalsh@ttu.ee>
2019-07-14 17:46:58 +0400

    Updates

---
+++ precache-manifest.6fbdc2edad11a5a90be892d85f210d6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ea608260c54e975d93fa362c000dce6",
+    "url": "/monster-workshop/index.html"
+  },
+  {
+    "revision": "3a18fdb0ff3350ae0119",
+    "url": "/monster-workshop/static/css/main.d09b064e.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monster-workshop/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3a18fdb0ff3350ae0119",
+    "url": "/monster-workshop/static/js/main.d1e461c0.chunk.js"
+  },
+  {
+    "revision": "8c1cd4b87b0afa381c2c",
+    "url": "/monster-workshop/static/js/runtime~main.fa3ebfd6.js"
+  }
+]);
