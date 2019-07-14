1eccfa09bc4ace196c573cae10e4fa68921b8fe0
Hakeem Shrief <abalsh@ttu.ee>
2019-07-14 10:03:17 +0400

    Add Constructor with Super to intialize State

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: []
+  }
+}
+
   render() {
     return (
       <div className="App">
