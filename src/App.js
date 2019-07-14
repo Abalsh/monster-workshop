31107436de0976b6359ece57bbd82870906e1094
Hakeem Shrief <abalsh@ttu.ee>
2019-07-14 11:22:04 +0400

    Implement CardList with Styles

--- src/App.js
+++ src/App.js
@@ -17,10 +17,11 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <CardList name="Test"/>
+        <CardList>
         {this.state.monsters.map(monster => (
           <h1 key={monster.id}> {monster.name} </h1>
         ))}
+        </CardList>
       </div>
     );
   }
