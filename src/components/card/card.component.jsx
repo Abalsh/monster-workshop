7013dac3a259c231b058163901202b2ec3cf2e74
Hakeem Shrief <abalsh@ttu.ee>
2019-07-14 11:45:23 +0400

    Add RoboHash Pictures

--- src/components/card/card.component.jsx
+++ src/components/card/card.component.jsx
@@ -4,7 +4,9 @@ import './card.styles.css';
 
 export const Card = (props) => (
     <div class='card-container'>
+        <img alt="monster" src={`https://robohash.org/${props.monster.id}?set=set2&size=180x180s`}/>
         <h1>{props.monster.name}</h1>
+            
     </div>
 )
 
